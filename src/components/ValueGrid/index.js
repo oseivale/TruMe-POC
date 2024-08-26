0a11af6a315e8461c06dad80488fcdb93945f054
Val O <valerie.osei@gmail.com>
2024-08-26 09:19:27 -0400

    initial branch commit, implementing canvas

--- src/components/ValueGrid/index.js
+++ src/components/ValueGrid/index.js
@@ -2,7 +2,9 @@
 
 import styles from "./styles.module.css";
 import { data } from "../../data";
-import { useState } from "react";
+import React, { useState, useRef } from "react";
+import { toPng, toJpeg } from "html-to-image";
+import "../../fonts/fonts.css";
 
 const ClearIcon = () => (
   <svg
@@ -20,22 +22,23 @@ const ClearIcon = () => (
 );
 
 export const ValueGrid = () => {
-    // Predefined colors for the user to choose from
-const colorOptions = ["#FF5733", "#33FF57", "#3357FF", "#FFD700", "#FF33A8"];
+  // Predefined colors for the user to choose from
+  const colorOptions = ["#FF5733", "#33FF57", "#3357FF", "#FFD700", "#FF33A8"];
 
-// Color palettes for the user to choose from
-const palettes = {
+  // Color palettes for the user to choose from
+  const palettes = {
     blue: ["#003f5c", "#2f4b7c", "#665191", "#a05195", "#d45087"],
     green: ["#006400", "#228B22", "#32CD32", "#7CFC00", "#ADFF2F"],
     red: ["#8B0000", "#B22222", "#DC143C", "#FF4500", "#FF6347"],
   };
 
-const [selectedColor, setSelectedColor] = useState(null); // To store the selected color
+  const [selectedColor, setSelectedColor] = useState(null); // To store the selected color
 
   const [selectedValues, setSelectedValues] = useState([]);
   const [singleColorMode, setSingleColorMode] = useState(false);
   const [selectedPalette, setSelectedPalette] = useState(null); // To store the selected palette
 
+  const logoRef = useRef(null); // Create a reference to the logo div
 
   // Custom order for mapping selected values to bars
   const customBarOrder = [2, 0, 1, 4, 3];
@@ -57,7 +60,7 @@ const [selectedColor, setSelectedColor] = useState(null); // To store the select
     setSelectedPalette(null);
   };
 
-const handleToggleSingleColor = () => {
+  const handleToggleSingleColor = () => {
     setSingleColorMode(!singleColorMode);
     if (!singleColorMode) {
       setSelectedColor(null); // Deselect the preset color if Single Color Mode is activated
@@ -71,7 +74,6 @@ const handleToggleSingleColor = () => {
       setSelectedColor(color);
       setSingleColorMode(false); // Turn off single color mode
       setSelectedPalette(null);
-
     }
   };
 
@@ -85,9 +87,42 @@ const handleToggleSingleColor = () => {
     }
   };
 
+  const handleDownloadImage = () => {
+    if (logoRef.current) {
+      // Get the dimensions of the logo element
+      const { width, height, top, left } =
+        logoRef.current.getBoundingClientRect();
+
+      setTimeout(() => {
+        toPng(logoRef.current, {
+          cacheBust: true,
+          width: Math.ceil(width), // Explicitly set the width
+          height: Math.ceil(height), // Explicitly set the height
+          style: {
+            transform: "none", // Disable any transforms for the capture
+            position: "static", // Temporarily reset the position for capture
+            top: 0, // Align to top
+            left: 0, // Align to left
+            width: `${Math.ceil(width)}px`, // Ensure width is correct
+            height: `${Math.ceil(height)}px`, // Ensure height is correct
+          },
+        })
+          .then((dataUrl) => {
+            const link = document.createElement("a");
+            link.download = "custom-logo.png";
+            link.href = dataUrl;
+            console.log("link: ", dataUrl);
+            link.click();
+          })
+          .catch((err) => {
+            console.error("Failed to export image", err);
+          });
+      }, 100); // Adjust the timeout as needed
+    }
+  };
 
   return (
-    <div style={{width: '90%', margin: '0 auto'}}>
+    <div style={{ width: "90%", margin: "0 auto" }}>
       <div style={{ marginBottom: "20px" }}>
         {Object.keys(data).map((key) => {
           const isSelected = selectedValues.includes(key);
@@ -152,45 +187,45 @@ const handleToggleSingleColor = () => {
             checked={singleColorMode}
             onChange={handleToggleSingleColor}
           />
-        
+
           <span className={styles.slider}></span>
         </label>
       </div>
 
-      <div style={{ marginBottom: '20px' }}>
-        <label style={{ marginRight: '10px' }}>Pick a Custom Color:</label>
+      <div style={{ marginBottom: "20px" }}>
+        <label style={{ marginRight: "10px" }}>Pick a Custom Color:</label>
         {colorOptions.map((color) => (
           <button
             key={color}
             onClick={() => handleColorSelection(color)}
             style={{
               backgroundColor: color,
-              width: '30px',
-              height: '30px',
-              margin: '0 5px',
-              border: selectedColor === color ? '3px solid #000' : 'none',
-              borderRadius: '50%',
-              cursor: 'pointer',
+              width: "30px",
+              height: "30px",
+              margin: "0 5px",
+              border: selectedColor === color ? "3px solid #000" : "none",
+              borderRadius: "50%",
+              cursor: "pointer",
             }}
           />
         ))}
       </div>
 
-      <div style={{ marginBottom: '20px' }}>
-        <label style={{ marginRight: '10px' }}>Pick a Palette:</label>
+      <div style={{ marginBottom: "20px" }}>
+        <label style={{ marginRight: "10px" }}>Pick a Palette:</label>
         {Object.keys(palettes).map((palette) => (
           <button
             key={palette}
             onClick={() => handlePaletteSelection(palette)}
             style={{
               backgroundColor: palettes[palette][2], // Displaying the middle color as the button color
-              width: '60px',
-              height: '30px',
-              margin: '0 5px',
-              border: selectedPalette === palette ? '3px solid #000' : 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              color: 'white'
+              width: "60px",
+              height: "30px",
+              margin: "0 5px",
+              border: selectedPalette === palette ? "3px solid #000" : "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              color: "white",
             }}
           >
             {palette.charAt(0).toUpperCase() + palette.slice(1)}
@@ -217,57 +252,78 @@ const handleToggleSingleColor = () => {
       </button>
 
       {/* Canvas for Displaying the Logo */}
+      <button
+        onClick={handleDownloadImage}
+        style={{
+          backgroundColor: "#120D96",
+          color: "#fff",
+          padding: "10px 20px",
+          marginTop: "20px",
+          borderRadius: "5px",
+          cursor: "pointer",
+          border: "none",
+        }}
+      >
+        Download Final Image
+      </button>
+
       <div
+        id="logo"
+        ref={logoRef} // Attach the reference to this div
         style={{
           display: "flex",
           alignItems: "flex-end",
           height: "350px",
           borderRadius: "10px",
           overflow: "hidden",
+          //   width: "430px",
           width: "400px",
-          margin: "30px auto",
+          position: "relative",
+          top: "0",
+          left: "30%",
+          // marginTop: '4rem'
+          //   margin: "30px auto",
         }}
       >
         {Array.from({ length: 5 }).map((_, index) => {
-  
+          // const barIndex = customBarOrder[index];
+          //   const selectedKey = selectedValues[barIndex];
+          //   const backgroundColor = singleColorMode
+          //     ? selectedValues.length > 1
+          //       ? data[selectedValues[0]].colourHex
+          //       : '#e0e0e0'
+          //     : selectedColor
+          //     ? selectedColor
+          //     : selectedKey
+          //     ? data[selectedKey].colourHex
+          //     : '#e0e0e0';
 
-        // const barIndex = customBarOrder[index];
-        //   const selectedKey = selectedValues[barIndex];
-        //   const backgroundColor = singleColorMode
-        //     ? selectedValues.length > 1
-        //       ? data[selectedValues[0]].colourHex
-        //       : '#e0e0e0'
-        //     : selectedColor
-        //     ? selectedColor
-        //     : selectedKey
-        //     ? data[selectedKey].colourHex
-        //     : '#e0e0e0';
+          // Determine the appropriate color for each bar
+          const barIndex = customBarOrder[index];
+          const selectedKey = selectedValues[barIndex];
 
-           // Determine the appropriate color for each bar
-           const barIndex = customBarOrder[index];
-           const selectedKey = selectedValues[barIndex];
- 
-           let backgroundColor;
- 
-           if (singleColorMode) {
-             // In single color mode, use the first selected value's color or the custom color
-             backgroundColor = selectedValues.length > 1 
-               ? data[selectedValues[0]].colourHex 
-               : selectedValues.length === 1 
-               ? data[selectedValues[0]].colourHex 
-               : '#e0e0e0';
-           } else if (selectedPalette) {
-             // If a palette is selected, use the corresponding color from the palette
-             backgroundColor = palettes[selectedPalette][index];
-           } else if (selectedColor) {
-             // Use the custom selected color if no palette is selected
-             backgroundColor = selectedColor;
-           } else {
-             // Default behavior for individual value selection
-             backgroundColor = selectedKey 
-               ? data[selectedKey].colourHex 
-               : '#e0e0e0';
-           }
+          let backgroundColor;
+
+          if (singleColorMode) {
+            // In single color mode, use the first selected value's color or the custom color
+            backgroundColor =
+              selectedValues.length > 1
+                ? data[selectedValues[0]].colourHex
+                : selectedValues.length === 1
+                ? data[selectedValues[0]].colourHex
+                : "#e0e0e0";
+          } else if (selectedPalette) {
+            // If a palette is selected, use the corresponding color from the palette
+            backgroundColor = palettes[selectedPalette][index];
+          } else if (selectedColor) {
+            // Use the custom selected color if no palette is selected
+            backgroundColor = selectedColor;
+          } else {
+            // Default behavior for individual value selection
+            backgroundColor = selectedKey
+              ? data[selectedKey].colourHex
+              : "#e0e0e0";
+          }
 
           return (
             <div
@@ -275,7 +331,7 @@ const handleToggleSingleColor = () => {
               className={styles[`option_${index}`]}
               style={{
                 flex: 1,
-            
+
                 backgroundColor: backgroundColor,
                 display: "flex",
                 flexDirection: "column",
@@ -294,13 +350,22 @@ const handleToggleSingleColor = () => {
                   <span
                     className={styles.coreValueLabel}
                     style={{
-                      fontSize: "24px",
+                      fontSize: "40px",
                       fontWeight: "bold",
-                      paddingBottom: "2rem",
+                      paddingBottom: ".55rem",
+                      //   margin:'2rem auto',
+                      display: "flex",
+                      alignItems: "center",
                     }}
                   >
-                    {data[selectedKey].shortCode}
+                    <h style={{ fontSize: "2rem" }}>
+                      {data[selectedKey].shortCode[0]}
+                    </h>
+                    <p style={{ fontSize: "1.3rem" }}>
+                      {data[selectedKey].shortCode[1]}
+                    </p>
                   </span>
+
                   {/* <span style={{ fontSize: '12px' }}>{data[selectedKey].name}</span> */}
                 </>
               ) : (
